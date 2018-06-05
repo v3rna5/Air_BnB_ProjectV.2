5ecae4063976132fd5760f1415c410fd0e84a284
verna <vernajs@gmail.com>
2018-06-05 16:43:30 -0700

    Finished Tues lesson

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Task } from './models/task.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    // selectedTask: Task = this.tasks[0];
+    this.selectedTask = null;
+  }
+}
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,5 @@
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public priority: number) {
+  }
+}
