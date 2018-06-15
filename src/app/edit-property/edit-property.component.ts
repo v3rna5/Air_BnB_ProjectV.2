fd18c154463d31c9cb2b589806d64e0b40cd322f
verna <vernajs@gmail.com>
2018-06-15 15:22:36 -0700

    Troubleshooting Inactive and Active function

--- src/app/edit-property/edit-property.component.ts
+++ src/app/edit-property/edit-property.component.ts
@@ -1,31 +1,6 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Component, Input, Output, EventEmitter } from '@angular/core';
-// import { Property } from '../models/property.model';
-//
-//
-// @Component({
-//   selector: 'app-edit-property',
-//   templateUrl: './edit-property.component.html',
-//   styleUrls: ['./edit-property.component.css']
-// })
-// export class EditTaskComponent implements OnInit {
-//
-//   constructor() { }
-//
-//   ngOnInit() {
-//   }
-//
-// }
-// export class EditPropertyComponent {
-//   @Input() childSelectedProperty: Property;
-//   @Output() clickedDone = new EventEmitter();
-//
-//   finishedEditing() {
-//      this.clickedDone.emit();
-//    }
-//  }
 
- import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+ //import { Component, Input, OnInit } from '@angular/core';
  import { PropertyService } from '../property.service';
 
  @Component({
--- src/app/property.model.ts
+++ src/app/property.model.ts
@@ -1,3 +1,4 @@
 export class Property {
+  public done: boolean = false;
   constructor (public condo: string, public address: string, public amenities: string, public price: string) { }
 }
